1e2eed3e6704c4cf859edad0b1602a9d44c7047e
Rajesh kumar Mohanta <rajeshkumarmohanta2001@gmail.com>
2023-10-26 16:56:34 +0530

    ghsdghsdhsb

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
+app.get("/rolldice", (req, res) => {
+  let rolldice = Math.floor(Math.random() * 6 + 1);
+  res.render("rolldice.ejs", { num: rolldice });
+});
+
+app.get("/ig/:username", (req, res) => {
+  let {username} = req.params;
+  const insta = require("./data.json")
+  const data = insta[username]
+  res.render("instagram.ejs",{data})
+  console.log(data);
+});
+
 let port = 35000;
 app.listen(port, () => {
   console.log(`Server is running ar port no ${port}`);
